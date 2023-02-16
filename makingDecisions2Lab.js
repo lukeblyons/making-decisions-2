92f414f85e5e8f8025c73652c94786fafd21347e
lukeblyons <lukeblyons@gmail.com>
2023-02-16 16:38:58 -0700

    All problems done

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -208,3 +208,15 @@ switch(letterGrade){
 */
 
 //Code Here
+
+for(let i = 0; i <=100; i++){
+  if(i % 3 === 0 && i % 5 != 0){
+    console.log('Dev');
+  }else if(i % 5 === 0 && i % 3 != 0){
+    console.log('mountian')
+  } else if(i % 3 === 0 && i % 5 === 0){
+    console.log('Devmountain');
+  } else{
+    console.log(i);
+  }
+}
